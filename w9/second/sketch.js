9005b301b759a5cca36f3f9e066cd3bcd2acc866
thubhyehyesuk <180945485+hyehyesuk@users.noreply.github.com>
2024-11-11 11:00:58 +0900

    https://github.com/hyehyesuk/IMD242_202220248

--- w9/second/sketch.js
+++ w9/second/sketch.js
@@ -1,6 +1,13 @@
+let x = 0;
+
 function setup() {
-  fullscreen(display);
+  fullScreen();
   background(0);
+  noStroke();
+  fill(102);
 }
 
-function draw() {}
+function draw() {
+  rect(x, height * 0.2, 1, height * 0.6);
+  x = x + 2;
+}
