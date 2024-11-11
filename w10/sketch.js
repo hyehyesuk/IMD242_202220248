bdff5a31e2b7a96f779d4cd64f89d06562639dbd
thubhyehyesuk <180945485+hyehyesuk@users.noreply.github.com>
2024-11-12 01:22:36 +0900

    https://github.com/hyehyesuk/IMD242_202220248

--- w10/sketch.js
+++ w10/sketch.js
@@ -4,8 +4,10 @@ function setup() {
   createCanvas(400, 400);
   capture = createCapture(VIDEO);
   console.log(capture);
+  capture.size(10, 10);
 }
 
 function draw() {
   background(220);
+  image(capture, 0, 0);
 }
