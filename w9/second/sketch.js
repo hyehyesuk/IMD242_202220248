99e83bc6548ffcbe074266c8ce3764782ac93148
thubhyehyesuk <180945485+hyehyesuk@users.noreply.github.com>
2024-11-11 16:21:21 +0900

    https://github.com/hyehyesuk/IMD242_202220248

--- w9/second/sketch.js
+++ w9/second/sketch.js
@@ -4,14 +4,4 @@ function setup() {
 
 function draw() {
   background('#333333');
-  noStroke();
-  fill('cornflowerblue');
-  for (let column = 0; column < width; column += 40) {
-    for (let row = 0; row < height; row += 40) {
-      let x = 20 + column;
-      let y = 20 + row;
-      let diameter = 30;
-      circle(x, y, diameter);
-    }
-  }
 }
